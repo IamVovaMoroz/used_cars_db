fb8bb01cd1b4fc8647720143687cc114ad423998
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-03-29 17:56:25 +0100

    report service

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -10,14 +10,13 @@ import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
 
-// ----
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) { }
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) { 
-    console.log(query)
+   return this.reportsService.createEstimate(query)
   }
 
   @Post()
